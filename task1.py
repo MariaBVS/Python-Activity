fff1ae556494c340fd9c3a5e1a569b95b9cef95b
MariaBVS <44699595+MariaBVS@users.noreply.github.com>
2018-11-19 17:36:48 +0100

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,62 @@
+""" TASK 1 GAME
+"""
+import random
+OPTIONS = ["r", "p", "s"]
+CHOISE = ["Rock", "Paper", "Scissors"]
+SCORE_CONTU = 0
+SCORE_CONTP = 0
+TRYS = 0
+print("Paper, Scissors, Rock  GAME")
+print("Think Fast!!!")
+def verify_input(user):
+    """ Check USER input  """
+    if user == 'r':
+        print("Your Choice: %s "%CHOISE[0])
+    elif user == 'p':
+        print("Your Choice: %s "%CHOISE[1])
+    elif user == 's':
+        print("Your Choice: %s "%CHOISE[2])
+while True:
+    USER = input("Choose your option ---> r (Rock) ---> p (Paper) ---> s (Scissors): ")
+    USER = USER.lower()
+    while USER != 'r' and USER != 'p' and USER != 's':
+        print("You introduced a Wrong value *** Please Try Again***")
+        USER = input("Choose your option ---> r (Rock) ---> p (Paper) ---> s (Scissors): ")
+    X = random.choice(OPTIONS)
+    verify_input(USER)
+    print("Computer choose: %s"%X)
+    if USER == X:
+        TRYS += 1
+        print("Try Again")
+        print("Socores: User:%d  Computer:%d"%(SCORE_CONTU, SCORE_CONTP))
+    elif USER == 'p' and X == 'r':
+        TRYS += 1
+        print("YOU WIN")
+        SCORE_CONTU += 1
+        print("Socores: User:%d  Computer:%d"%(SCORE_CONTU, SCORE_CONTP))
+    elif USER == 'r' and X == 's':
+        TRYS += 1
+        print("YOU WIN")
+        SCORE_CONTU += 1
+        print("Socores: User:%d  Computer:%d"%(SCORE_CONTU, SCORE_CONTP))
+    elif USER == 's' and X == 'p':
+        TRYS += 1
+        print("YOU WIN")
+        SCORE_CONTU += 1
+        print("Socores: User:%d  Computer:%d"%(SCORE_CONTU, SCORE_CONTP))
+    else:
+        TRYS += 1
+        print("Computer WIN")
+        SCORE_CONTP += 1
+        print("Socores: User:%d  Computer:%d"%(SCORE_CONTU, SCORE_CONTP))
+    if SCORE_CONTU == 2:
+        print("CONGRATULATIONS!!! YOU ARE THE WINER")
+        print("FINAL SCORE: User:%d  Computer:%d"%(SCORE_CONTU, SCORE_CONTP))
+        break
+    elif SCORE_CONTP == 2:
+        print("COMPUTER BEATS YOU")
+        print("FINAL SCORE: User:%d  Computer:%d "%(SCORE_CONTU, SCORE_CONTP))
+        break
+    elif TRYS == 3:
+        print("You reached Three intentd----Nobody WIN try again")
+        break
